b26db2f080d26bd1db0a3585f2571a16af7f122b
formesdesign <72689061+formesdesign@users.noreply.github.com>
2020-11-19 19:46:52 +0100

    Add files via upload

--- romanos.py
+++ romanos.py
@@ -1,12 +1,11 @@
-#creen un diccionari
+#creen un diccionari simbolos
 simbolos = {"M":1000, "D":500, "C": 100, "L":50, "X":10, "V":5, "I":1}
 tipo_5 = ("V", "D", "L")
-tipo_1 = ("I", "X", "C", "M")
 restas = ("CD", "CM", "XL", "XC", "IV", "IX")
 
 def simbolo_a_entero(simbolo):
     if isinstance(simbolo, str) and simbolo.upper() in simbolos: #upper es per forçar les mayusculas
-            #isinstance, es per comprovar si una variable es d'un tipo especific, i sempre en dos paramentres
+    #isinstance, es per comprovar si una variable es d'un tipo específic, i sempre en dos paramentres
         return simbolos[simbolo.upper()]
     elif isinstance(simbolo, str):
         raise ValueError(f"simbolo {simbolo} no permitido") #cadenas con formato
@@ -14,32 +13,61 @@ def simbolo_a_entero(simbolo):
     else:
         raise ValueError(f"parametro {simbolo} debe ser un string")
 
+def orden_magnitud(caracter):
+    valor = simbolo_a_entero(caracter)
+    return len(str(valor))
+   
 def romano_a_entero(romano):
     if not isinstance(romano, str):
         raise ValueError(f"parametro {romano} debe ser un string")
 
     suma = 0
     c_repes = 0
-    valor_anterior = "" #valor anterior no vale nada primer i depues se acumula
+    valor_anterior = "" #valor anterior no val res: primer i depueés s'acumula
+    orden_magnitud_global = 0
+    orden_magnitud_letra = 0
+    ha_habido_resta = False
 
     for letra in romano:
-        if letra == valor_anterior and letra in tipo_5:
-            raise OverflowError (f"Demasiado simbolos de tipo{letra}")
-        if letra == valor_anterior:
-            c_repes +=1
-            if c_repes > 2:
-                raise OverflowError (f"Demasiado simbolos de tipo{letra}")
-            #overflowerror: massa quantitat desvordament
-        elif valor_anterior and simbolo_a_entero(letra) > simbolo_a_entero(valor_anterior):
-            if valor_anterior + letra not in restas:
-                raise ValueError
-            suma -= simbolos [valor_anterior] * 2 #suma i resta dos vegades per sumar al següent lletra, xq el valor del davant es més peque
-            c_repes = 0
-        else:
-            c_repes = 0 #es per la lletra diferent i torne a contar a 0
+            orden_magnitud_letra = orden_magnitud(letra)
+            if letra == valor_anterior:
+                orden_magnitud_global = orden_magnitud_letra
+                if letra in tipo_5:
+                    raise ValueError("No és romano")
+                elif c_repes >= 2:
+                    raise ValueError("Massa repeticions")
+                elif ha_habido_resta:
+                    raise ValueError("Massa restes")
+                c_repes += 1
+            elif valor_anterior and simbolo_a_entero(letra) > simbolo_a_entero(valor_anterior):
+                if valor_anterior + letra not in restas:
+                    raise ValueError("Resta no permessa")
+                elif c_repes > 0:
+                    raise ValueError("Resta tras repetició no permesa")
+                elif ha_habido_resta:
+                    raise ValueError("Massa restes")
 
-        suma = suma + simbolo_a_entero(letra)
-        valor_anterior = letra
+                ha_habido_resta = True
+                suma -= 2*simbolo_a_entero(valor_anterior)
+                #suma i resta dos vegades per sumar al següent lletra, xq el valor del davant es més peque
+                c_repes = 0
+            else:
+                if orden_magnitud_global > orden_magnitud_letra:
+                    ha_habido_resta = False
+
+                if ha_habido_resta:
+                    raise ValueError("Massa restes")
+
+                orden_magnitud_global = orden_magnitud_letra
+                c_repes = 0 #es per la lletra diferent i torne a contar a 0
+
+            suma = suma + simbolo_a_entero(letra)
+            valor_anterior = letra    
     return suma
+
+
+
+    #overflowerror: massa quantitat desvordament         
+
 
 
